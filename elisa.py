6b5400a96033d13b9a65072bff925282ba217c6c
Emeline Padié <emeline.padie@etu.minesparis.psl.eu>
2024-01-23 15:04:51 +0100

    je veux pull

--- elisa.py
+++ elisa.py
@@ -23,9 +23,9 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 ## on se connecte selon que c'est un client ou un serveur
 if args.c == True:
     s.connect(('localhost', 34000))  #a utiliser quand on est le client
-    s.sock.send('hello')
+    s.sock.send(b'hello')
 else:
     s.bind(('',34000))      #a utiliser quand on est le serveur
     s.listen()
     (client_socket, adrr) = s.accept()
-    s.sock.recv('hi')
+    s.sock.recv(b'hi')
