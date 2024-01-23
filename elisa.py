3db8cc3b5b5559cc8a3a1d30a8d4d5e5128f32a2
Elisa Skowronek <elisa.skowronek@etu.minesparis.psl.eu>
2024-01-23 15:49:06 +0100

    logger

--- elisa.py
+++ elisa.py
@@ -6,13 +6,22 @@
 
 import socket
 import argparse
+import sys
+import logging
 
 ##Arguments
 parser = argparse.ArgumentParser(description='Some description.')
 
 parser.add_argument('-c',help='A flag. When True, is a client', action='store_true')
+parser.add_argument('-host', default='localhost', help='The host to connect to')
 
 args = parser.parse_args()
+
+##logging
+logger=logging.getLogger(__name__)
+handler=logging.StreamHandler(sys.stderr)
+logger.addHandler(handler)
+logger.setLevel(logging.DEBUG)
 
 HANDSHAKE_MSG = b'hello'
 HANDSHAKE_REPLY = b'hi'
@@ -22,14 +31,17 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 ## on se connecte selon que c'est un client ou un serveur
 if args.c == True:
-    s.connect(('localhost', 34000))  #a utiliser quand on est le client
-    print('connected')
+    host=args.host
+    s.connect((host, 34000))  #a utiliser quand on est le client
+    logger.debug('connected')
     s.send(HANDSHAKE_MSG)
-    print('sent')
+    logger.debug('sent handshake')
 else:
     s.bind(('',34000))      #a utiliser quand on est le serveur
     s.listen()
     (client_socket, adrr) = s.accept()
-    print('accepted')
+    logger.debug('accepted connection')
     client_socket.recv(len(HANDSHAKE_MSG))
+    logger.debug('received handshake')
     client_socket.send(HANDSHAKE_REPLY)
+    logger.debug('sent handshake reply')
